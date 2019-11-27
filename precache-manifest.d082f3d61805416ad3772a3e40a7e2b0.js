3afa9ea6cb9179e63ad3f50775a57008ff7bb625
Artyom Ganev <a.a.ganev@gmail.com>
2019-11-27 17:22:51 +0300

    Updates

---
+++ precache-manifest.d082f3d61805416ad3772a3e40a7e2b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb10039b276d76f7b57cb48dbb1db9de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93c906e5b1dee83a617a",
+    "url": "/static/css/main.7e1bf04f.chunk.css"
+  },
+  {
+    "revision": "fc83544bbf302cbcc100",
+    "url": "/static/js/2.68c64642.chunk.js"
+  },
+  {
+    "revision": "93c906e5b1dee83a617a",
+    "url": "/static/js/main.3b156cdc.chunk.js"
+  },
+  {
+    "revision": "4e1d5a340060b616e288",
+    "url": "/static/js/runtime-main.dd45831b.js"
+  },
+  {
+    "revision": "bbb441b28eda7231ca7dfbf56787a393",
+    "url": "/static/media/artyom.bbb441b2.jpg"
+  },
+  {
+    "revision": "47f71852355abed89f88524b5e79cc86",
+    "url": "/static/media/rybinsk.47f71852.jpg"
+  }
+]);

282fe93a414f3fcf86fffd34d25fd8a01aba3ef2
aa.ganev <aa.ganev@tensor.ru>
2019-12-10 18:25:18 +0300

    add photo gallery

--- src/components/Page/Gallery.tsx
+++ src/components/Page/Gallery.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import axios, { AxiosResponse } from 'axios';
+import React, { ReactNode } from 'react';
+import LoadingIndicator from '../Navigation/LoadingIndicator';
+import Container from './Container';
 
-const BASE_URL = 'https://lh3.googleusercontent.com/hWOmOcWH5ofhbkbjnjJh1dXQC8Yc1D0IZRnOdPFotOiK8jZH7VTqUV-56KSmX5sZzvkSfcTH_giJ_8UGvdsEPAQP3DKkyOxzTqqq9J_csg';
 
-export default () => {
-   return <img src={BASE_URL} alt=""/>;
+export default class Gallery extends React.Component {
+   public state: { items: any[] } = { items: [] };
+   private loaded: boolean = false;
+
+   public componentDidMount(): void {
+      this.getList().then((items: any[]) => {
+         this.loaded = true;
+         this.setState({ items });
+      });
+   }
+
+   public render(): ReactNode {
+      const content = this.loaded ? this.getContent() : <LoadingIndicator/>;
+      return <Container title="Gallery" content={content}/>;
+   }
+
+   private getList(): Promise<any> {
+      return new Promise((resolve) => {
+         axios.get<any>('https://photoslibrary.googleapis.com/v1/albums', {
+            params: {
+               key: ''
+            }
+         })
+            .then(({ data }: AxiosResponse<any>) => {
+               resolve(data);
+            })
+            .catch(() => resolve([]));
+      });
+   }
+
+   private getContent(): ReactNode {
+      return <div className='flexBox flexColumn'>{this.state.items}</div>;
+   }
 }

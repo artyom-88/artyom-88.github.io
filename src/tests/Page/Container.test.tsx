586debe777830718e9e04d74c0d2f6d62d6c4635
Artyom Ganev <a.a.ganev@gmail.com>
2020-01-18 14:14:23 +0300

    https://github.com/Artyom-Ganev/artyom-ganev.github.io/issues/1

--- src/tests/Page/Container.test.tsx
+++ src/tests/Page/Container.test.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Container from '../../components/Page/Container';
 
-test('Container 1', () => {
+test('Container', () => {
     const title = 'title 1';
     const content = <div>text1</div>;
     const component = renderer.create(<Container title={title} content={content} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('Container', () => {
+    const content = <div>text1</div>;
+    const component = renderer.create(<Container content={content} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });

ab58a93a72b0020e2a40603418350d9d5b41174f
Artem_Ganev <a.a.ganev@gmail.com>
2020-02-13 13:31:26 +0300

    https://github.com/Artyom-Ganev/artyom-ganev.github.io/issues/4 Add React hooks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,72 +1,60 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
+import './App.scss';
 import Menu from './Navigation/Menu';
-import Main from './Page/Main';
 import About from './Page/About';
 import Blog from './Page/Blog';
 import Career from './Page/Career';
 import Contacts from './Page/Contacts';
+import Main from './Page/Main';
 import NotFound from './Page/NotFound';
-import './App.scss';
-import { connect } from 'react-redux';
-import IState from '../interface/IState';
-import * as actions from '../actions';
-
-interface IProperties {
-  narrow: boolean;
-  appResize: (payload: { narrow: boolean }) => void;
-}
 
 /**
  * Check if the page is narrow
  */
 const isNarrow = () => window.innerWidth <= 800;
 
-const mapStateToProps = ({ app: { narrow } }: IState) => ({ narrow });
-
-const actionCreators = {
-  appResize: actions.appResize,
-};
+export const NarrowContext = React.createContext(isNarrow());
 
 /**
  * Main application component
  */
-class App extends Component<IProperties> {
-  public componentDidMount() {
-    // init size state
-    this.onResize();
-    window.addEventListener('resize', this.onResize);
-  }
+const App = () => {
+  const [, setNarrow] = useState<boolean>(isNarrow());
 
-  public componentWillUnmount() {
-    window.removeEventListener('resize', this.onResize);
-  }
+  useEffect(() => {
+    const onResize = () => setNarrow(isNarrow());
+    window.addEventListener('resize', onResize);
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  });
 
-  render = () => (
+  return (
     <HashRouter>
       <div className='flexBox flexColumn'>
-        <header className='flexBox'>
-          <Menu />
-        </header>
-        <div className='components-app__background' />
-        <div className='flexBox flexColumn components-app__content'>
-          <Switch>
-            <Redirect from='/main' to='/' />
-            <Route exact={true} path='/' component={Main} />
-            <Route exact={true} path='/about' component={About} />
-            <Route exact={true} path='/blog' component={Blog} />
-            <Route exact={true} path='/career' component={Career} />
+        <NarrowContext.Provider value={isNarrow()}>
+          <header className='flexBox'>
+            <Menu />
+          </header>
+          <div className='components-app__background' />
+          <div className='flexBox flexColumn components-app__content'>
+            <Switch>
+              <Redirect from='/main' to='/' />
+              <Route exact={true} path='/' component={Main} />
+              <Route exact={true} path='/about' component={About} />
+              <Route exact={true} path='/blog' component={Blog} />
+              <Route exact={true} path='/career' component={Career} />
 
-            <Route exact={true} path='/contacts' component={Contacts} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-        <footer className='components-app__rights'>© 2019 All rights reserved</footer>
+              <Route exact={true} path='/contacts' component={Contacts} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+          <footer className='components-app__rights'>© 2019 All rights reserved</footer>
+        </NarrowContext.Provider>
       </div>
     </HashRouter>
   );
+};
 
-  private onResize = () => this.props.appResize({ narrow: isNarrow() });
-}
-
-export default connect(mapStateToProps, actionCreators)(App);
+export default App;

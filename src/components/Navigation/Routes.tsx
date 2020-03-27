55225237b6d3639234f6a812163a23fc95fc74e4
Artem_Ganev <a.a.ganev@gmail.com>
2020-03-27 17:45:33 +0300

    Fix code style

--- src/components/Navigation/Routes.tsx
+++ src/components/Navigation/Routes.tsx
@@ -8,13 +8,13 @@ import { About, Blog, Career, Contacts, Main, NotFound } from 'components/Pages'
  */
 const Routes: FunctionComponent = () => (
   <Switch>
-      <Redirect from='/main' to='/' />
-      <Route exact={true} path='/' component={Main} />
-      <Route exact={true} path='/about' component={About} />
-      <Route exact={true} path='/blog' component={Blog} />
-      <Route exact={true} path='/career' component={Career} />
-      <Route exact={true} path='/contacts' component={Contacts} />
-      <Route component={NotFound} />
+    <Redirect from='/main' to='/' />
+    <Route exact={true} path='/' component={Main} />
+    <Route exact={true} path='/about' component={About} />
+    <Route exact={true} path='/blog' component={Blog} />
+    <Route exact={true} path='/career' component={Career} />
+    <Route exact={true} path='/contacts' component={Contacts} />
+    <Route component={NotFound} />
   </Switch>
 );
 

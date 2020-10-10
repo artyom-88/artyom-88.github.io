efac95dc3f5aa6c43c53d956f421db2c06435341
Artem_Ganev <a.a.ganev@gmail.com>
2020-10-10 20:51:47 +0300

    2020-10-10 Update: Update dependencies. Migrate to MongoDB Atlas

--- src/components/Pages/MainPageContent.tsx
+++ src/components/Pages/MainPageContent.tsx
@@ -13,6 +13,8 @@ const LINKS = {
   Rybinsk: 'https://en.wikipedia.org/wiki/Rybinsk',
 };
 
+const TOOLTIP = 'Click to see sources';
+
 const MainPageContent: FC = () => {
   const classes = useStyles();
   return (
@@ -24,29 +26,25 @@ const MainPageContent: FC = () => {
         </Typography>
         <Typography className={classes.mainPageBlock} paragraph variant='h6'>
           I&apos;m a programmer, based in&nbsp;
-          <a href={LINKS.Rybinsk} target={BLANK} rel={REL}>
+          <a href={LINKS.Rybinsk} target={BLANK} rel={REL} title='Click to read about Rybinsk on Wikipedia'>
             Rybinsk
           </a>
           ,&nbsp;Russia. There are some facts about me on this site. Use &quot;MENU&quot; button to navigate.
         </Typography>
         <Typography className={classes.mainPageBlock} paragraph variant='h6'>
-          Front-End of this site is based on React, Redux, TypeScript and Material UI, the sources of this part are
-          located on GitHub&nbsp;
-          <a href={LINKS.Front} target={BLANK} rel={REL}>
-            here
+          <a href={LINKS.Front} target={BLANK} rel={REL} title={TOOLTIP}>
+            Front-End
           </a>
-          .
+          &nbsp;of the site is based on React, Redux, TypeScript and Material UI.
         </Typography>
         <Typography className={classes.mainPageBlock} paragraph variant='h6'>
-          Back-End uses Nest.js, Mongo DB, Heroku and is located&nbsp;
-          <a href={LINKS.Back} target={BLANK} rel={REL}>
-            here
+          <a href={LINKS.Back} target={BLANK} rel={REL} title={TOOLTIP}>
+            Back-End
           </a>
-          .
+          &nbsp;application is developed using Nest.js, Heroku and Mongo DB Atlas.
         </Typography>
         <Typography className={classes.mainPageBlock} paragraph variant='h6'>
-          You can contact me if you have some ideas for it&apos;s improvement or if you find some bugs, typos etc.
-          I&apos;m still working on it.
+          You can contact me if you have some ideas for it&apos;s improvement or if you find some bugs, typos, etc.
         </Typography>
       </CardContent>
     </Card>

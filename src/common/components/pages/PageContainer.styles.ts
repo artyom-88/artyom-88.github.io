3547bd59a6747137436f9876ab59bf5b53b5b879
Artem Ganev <a.a.ganev@gmail.com>
2022-02-04 20:42:22 +0300

    Release/2022 02 04 (#672)
    * 2022-02-04 Update: Migrate to Material UI 5.
    * Fix PageContainer padding

--- src/common/components/pages/PageContainer.styles.ts
+++ src/common/components/pages/PageContainer.styles.ts
@@ -1,15 +1,14 @@
 import { Theme } from '@mui/material/styles';
 import makeStyles from '@mui/styles/makeStyles';
-import { FOOTER_HEIGHT_SPACING } from 'common/const/layout.const';
-
-const paddingTopSpacing = 2;
+import { FOOTER_HEIGHT_SPACING, HEADER_HEIGHT_SPACING } from 'common/const/layout.const';
 
 export default makeStyles((theme: Theme) => ({
   pageContainer: {
-    height: `calc(100% - ${theme.spacing(FOOTER_HEIGHT_SPACING + paddingTopSpacing)})`,
-    overflow: 'auto',
-    paddingTop: theme.spacing(paddingTopSpacing),
+    height: `calc(100% - ${theme.spacing(FOOTER_HEIGHT_SPACING + HEADER_HEIGHT_SPACING)})`,
     opacity: '0.8',
+    overflow: 'auto',
+    paddingBottom: theme.spacing(2),
+    paddingTop: theme.spacing(2),
   },
   pageContainerTitle: {
     marginLeft: theme.spacing(1),

88381dda9e0e07ac13e2f99287769150b12966a5
Artem Ganev <a.a.ganev@gmail.com>
2022-03-27 17:48:22 +0300

    re-sync with `master`

--- src/common/components/icons/FacebookIcon.tsx
+++
@@ -1,6 +0,0 @@
-import Facebook from '@mui/icons-material/Facebook';
-import { FC, ReactElement } from 'react';
-
-const FacebookIcon: FC = (): ReactElement => <Facebook htmlColor='#1194F4' />;
-
-export default FacebookIcon;

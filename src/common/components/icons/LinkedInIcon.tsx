c6b1bdedec469005de8910142284448a9113fed1
Artem Ganev <a.a.ganev@gmail.com>
2022-03-27 17:19:12 +0300

    Remove unused stuff after sync with `master`

--- src/common/components/icons/LinkedInIcon.tsx
+++
@@ -1,6 +0,0 @@
-import LinkedIn from '@mui/icons-material/LinkedIn';
-import { FC, ReactElement } from 'react';
-
-const LinkedInIcon: FC = (): ReactElement => <LinkedIn htmlColor='#0A66C2' />;
-
-export default LinkedInIcon;

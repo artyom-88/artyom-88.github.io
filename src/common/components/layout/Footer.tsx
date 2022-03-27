a96a14fcfc1c96ec3f177854b2c240f63bbd642c
Artem Ganev <a.a.ganev@gmail.com>
2022-03-27 17:02:02 +0300

    Footer redesign

--- src/common/components/layout/Footer.tsx
+++ src/common/components/layout/Footer.tsx
@@ -1,45 +1,21 @@
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import Container from '@mui/material/Container';
-import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { ComponentWithClassName } from 'common/types/common.types';
-import { openWindow } from 'common/utils/navigate.utils';
-import { ReactElement, SyntheticEvent, useCallback } from 'react';
-import { FOOTER_LINKS, RIGHTS_TEXT } from './Footer.constants';
-import { FooterLinks } from './Footer.types';
+import { ReactElement } from 'react';
 
-const Footer = ({ className }: ComponentWithClassName): ReactElement => {
-  const onLinkClick = useCallback((e: SyntheticEvent, value: FooterLinks) => {
-    const url = FOOTER_LINKS[value].url;
-    openWindow(url);
-  }, []);
+export const RIGHTS_TEXT = `© ${new Date().getFullYear()} All rights reserved`;
 
-  return (
-    <Container className={className} component='footer' disableGutters fixed>
-      <Paper square>
-        <BottomNavigation onChange={onLinkClick}>
-          {Object.entries(FOOTER_LINKS).map(([k, { Icon }]) => (
-            <BottomNavigationAction
-              key={k}
-              value={k}
-              icon={
-                <IconButton>
-                  <Icon />
-                </IconButton>
-              }
-            />
-          ))}
-        </BottomNavigation>
-        <div className='ag-flexbox ag-justifyContent_center ag-alignItems_center'>
-          <Typography color='textSecondary' fontWeight='bold'>
-            {RIGHTS_TEXT}
-          </Typography>
-        </div>
-      </Paper>
-    </Container>
-  );
-};
+const Footer = ({ className }: ComponentWithClassName): ReactElement => (
+  <Container className={className} component='footer' disableGutters fixed>
+    <Paper square>
+      <div className='ag-flexbox ag-justifyContent_center ag-alignItems_center'>
+        <Typography color='textSecondary' fontWeight='bold'>
+          {RIGHTS_TEXT}
+        </Typography>
+      </div>
+    </Paper>
+  </Container>
+);
 
 export default Footer;

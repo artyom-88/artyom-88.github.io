229dd2ef1007ebe847525cced99a57eb535f1a40
Artem Ganev <a.a.ganev@gmail.com>
2023-12-14 18:33:08 -0500

    Use timeline on career page

--- src/features/career/Career.tsx
+++ src/features/career/Career.tsx
@@ -1,34 +1,27 @@
-import { ReactElement } from 'react';
+import { ReactElement, useMemo } from 'react';
 
-import Col from 'antd/es/col';
-import Row from 'antd/es/row';
-import Typography from 'antd/es/typography';
+import Timeline, { TimelineItemProps } from 'antd/es/timeline';
 
 import PageContainer from 'common/components/PageContainer';
 import { CAREER_PAGE_PROPS } from 'common/constants/pages-constants';
-import { CAREER_ROW_GUTTER } from 'features/career/career-constants';
 import CareerItem from 'features/career/CareerItem';
+import CareerItemTitle from 'features/career/CareerItemTitle';
 import { useCareerListQuery } from 'features/career/hooks/use-career-list-query';
 
 const Career = (): ReactElement => {
   const { data: list = [], isLoading } = useCareerListQuery();
+  const timelineItems = useMemo<TimelineItemProps[]>(
+    () =>
+      list.map((item, index) => ({
+        children: <CareerItem item={item} />,
+        color: index ? 'blue' : 'green',
+        label: <CareerItemTitle item={item} />,
+      })),
+    [list],
+  );
   return (
     <PageContainer isLoading={isLoading} title={CAREER_PAGE_PROPS.id} icon={CAREER_PAGE_PROPS.icon}>
-      <Row gutter={CAREER_ROW_GUTTER} wrap={false}>
-        <Col span={10} xs={0} sm={0} md={0} lg={14} xl={11} xxl={10}>
-          <Typography.Title className='mt-0' level={5}>
-            Description
-          </Typography.Title>
-        </Col>
-        <Col span={14} xs={0} sm={0} md={0} lg={10} xl={13} xxl={14}>
-          <Typography.Title className='mt-0' level={5}>
-            Technologies and tools
-          </Typography.Title>
-        </Col>
-      </Row>
-      {list.map((item) => (
-        <CareerItem item={item} key={item._id} />
-      ))}
+      <Timeline items={timelineItems} mode='left' />
     </PageContainer>
   );
 };
--- src/features/career/CareerItem.tsx
+++ src/features/career/CareerItem.tsx
@@ -1,35 +1,15 @@
 import type { JSX } from 'react';
 
-import Col from 'antd/es/col';
-import Row from 'antd/es/row';
 import Space from 'antd/es/space';
 import Tag from 'antd/es/tag';
 import Typography from 'antd/es/typography';
 
-import { BLANK, REL } from 'common/constants/html-constants';
-import { CAREER_ROW_GUTTER } from 'features/career/career-constants';
 import { CareerItemProps } from 'features/career/career-types';
 
 const CareerItem = ({ item }: CareerItemProps): JSX.Element => {
-  const { _id: id, site = '', title, post, description, tools, startDate, endDate } = item;
-  const startDateText = startDate.format('MMMM Do, YYYY');
-  const endDateText = endDate.isValid() ? endDate.format('MMMM Do, YYYY') : '';
-  const postRender = <Typography.Text strong>{post}</Typography.Text>;
-  const dateRender = (
-    <Typography.Text strong>{endDateText ? `${startDateText} - ${endDateText}` : `Since ${startDateText}`}</Typography.Text>
-  );
+  const { post, description, tools } = item;
   const descriptionRender = <div>{description}</div>;
-  const companyRender = (
-    <div>
-      {site ? (
-        <a href={site} rel={REL} target={BLANK}>
-          {title}
-        </a>
-      ) : (
-        title
-      )}
-    </div>
-  );
+  const postRender = <Typography.Text strong>{post}</Typography.Text>;
   const toolsRender = (
     <Space wrap>
       {tools.map((tool) => (
@@ -38,32 +18,11 @@ const CareerItem = ({ item }: CareerItemProps): JSX.Element => {
     </Space>
   );
   return (
-    <Row key={id} gutter={CAREER_ROW_GUTTER} wrap={false}>
-      <Col span={4} xs={0} sm={0} md={0} lg={5} xl={4} xxl={4}>
-        <Space direction='vertical'>
-          {postRender}
-          {companyRender}
-        </Space>
-      </Col>
-      <Col span={6} xs={0} sm={0} md={0} lg={9} xl={7} xxl={6}>
-        <Space direction='vertical'>
-          {dateRender}
-          {descriptionRender}
-        </Space>
-      </Col>
-      <Col xs={0} span={14} sm={0} md={0} lg={10} xl={13} xxl={14}>
-        {toolsRender}
-      </Col>
-      <Col span={0} xs={24} sm={24} md={24} lg={0}>
-        <Space wrap>
-          {companyRender}
-          {dateRender}
-          {postRender}
-          {descriptionRender}
-          {toolsRender}
-        </Space>
-      </Col>
-    </Row>
+    <Space direction='vertical'>
+      {postRender}
+      {descriptionRender}
+      {toolsRender}
+    </Space>
   );
 };
 

a7aee1ac11addf2f9354e2d9be1177b71c53cd94
Artem Ganev <a.a.ganev@gmail.com>
2024-04-01 13:24:52 +0000

    9708dce3436955c82df04a250aadd806ae27628d

---
+++ assets/http-client-_xH_jDZn.js
@@ -0,0 +1,1 @@
+import{m as e}from"./vendors-5jLEKWBm.js";const a=e.create({headers:{"Content-Type":"application/json"},prefixUrl:"https://artemganev-be-13b50ea004b5.herokuapp.com/api",retry:0});export{a as h};
--- assets/http-client-cnw8xU3k.js
+++
@@ -1,1 +0,0 @@
-import{m as e}from"./vendors-RqhVJaCb.js";const a=e.create({headers:{"Content-Type":"application/json"},prefixUrl:"https://artemganev-be-13b50ea004b5.herokuapp.com/api",retry:0});export{a as h};

941f136479615bbeb3646afd30d51a1c751aa778
Artёm <artem_ganev@epam.com>
2024-11-16 14:22:35 -0500

    Fix/hide warning after loaded (#1176)
    * Update NodeJS to v.22
    
    * Fix PNPM install
    
    * Hide Heroku warning after data loaded
    
    * Add more e2e tests

--- src/test/e2e/blog.spec.ts
+++ src/test/e2e/blog.spec.ts
@@ -1,8 +1,25 @@
 import { expect, test } from '@playwright/test';
 
+import { BlogDTO } from 'features/blog/blog-types';
+
 test.describe('blog page', () => {
-  test('has title', async ({ page }) => {
+  test('has default content', async ({ page }) => {
     await page.goto('/#/blog');
     await expect(page).toHaveTitle('blog');
+    await expect(page.getByTestId('nav-menu')).toBeVisible();
+    await expect(page.getByText('blog')).toBeVisible();
+  });
+
+  test('has blog item list', async ({ page }) => {
+    const [response] = await Promise.all([
+      page.waitForResponse((res) => res.url().includes('/api/blog') && res.status() === 200, { timeout: 30000 }),
+      page.goto('/#/blog'),
+    ]);
+    const blogItems: BlogDTO[] = await response.json();
+    await Promise.all(
+      blogItems.map(async ({ _id: id }) => {
+        await expect(page.getByTestId(`blog-item-${id}`)).toBeVisible();
+      }),
+    );
   });
 });

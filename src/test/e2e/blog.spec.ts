512e4ed23b09a46e3ef34d81b8f439343e4204ff
Artem Ganev <artem_ganev@epam.com>
2024-11-16 16:04:11 -0500

    Debug

--- src/test/e2e/blog.spec.ts
+++ src/test/e2e/blog.spec.ts
@@ -6,11 +6,11 @@ test.describe('blog page', () => {
   test('has default content', async ({ page }) => {
     console.log('has default start');
     await page.goto('/#/blog');
-    console.log(page.url());
+    console.log('has default start', page.url());
     await expect(page).toHaveTitle('blog');
     await expect(page.getByTestId('nav-menu')).toBeVisible();
     await expect(page.getByText('blog')).toBeVisible();
-    console.log('success');
+    console.log('has default success');
   });
 
   test('has blog item list', async ({ page }) => {
@@ -19,13 +19,13 @@ test.describe('blog page', () => {
       page.waitForResponse((res) => res.url().includes('/api/blog') && res.status() === 200, { timeout: 60000 }),
       page.goto('/#/blog'),
     ]);
-    console.log(page.url());
+    console.log('has blog item list', page.url());
     const blogItems: BlogDTO[] = await response.json();
     await Promise.all(
       blogItems.map(async ({ _id: id }) => {
         await expect(page.getByTestId(`blog-item-${id}`)).toBeVisible();
       }),
     );
-    console.log('success');
+    console.log('has blog item list success');
   });
 });

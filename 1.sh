8292cd4ecc359f134687c2babddeeaa3d4a9c893
Artem Ganev <artem_ganev@epam.com>
2024-11-16 18:21:19 -0500

    Fix version usage

--- 1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-VERSION_OUTPUT=$(pnpm exec playwright --version)
-PLAYWRIGHT_VERSION=$(echo $VERSION_OUTPUT | grep -o '[0-9]\+\.[0-9]\+' | head -1)
-echo "PLAYWRIGHT_VERSION=$PLAYWRIGHT_VERSION"

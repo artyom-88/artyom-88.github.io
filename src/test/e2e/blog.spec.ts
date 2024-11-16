dcdcc6697e9d2a537a41f8c01a6a32f9a151616d
Artem Ganev <artem_ganev@epam.com>
2024-11-16 16:37:18 -0500

    Add API url to playwright job

--- src/test/e2e/blog.spec.ts
+++ src/test/e2e/blog.spec.ts
@@ -4,28 +4,22 @@ import { BlogDTO } from 'features/blog/blog-types';
 
 test.describe('blog page', () => {
   test('has default content', async ({ page }) => {
-    console.log('has default start');
     await page.goto('/#/blog');
-    console.log('has default', page.url());
     await expect(page).toHaveTitle('blog');
     await expect(page.getByTestId('nav-menu')).toBeVisible();
     await expect(page.getByText('blog')).toBeVisible();
-    console.log('has default success');
   });
 
   test('has blog item list', async ({ page }) => {
-    console.log('has blog item list start');
     const [response] = await Promise.all([
       page.waitForResponse((res) => res.url().includes('/api/blog') && res.status() === 200, { timeout: 5000 }),
       page.goto('/#/blog'),
     ]);
-    console.log('has blog item list', page.url());
     const blogItems: BlogDTO[] = await response.json();
     await Promise.all(
       blogItems.map(async ({ _id: id }) => {
         await expect(page.getByTestId(`blog-item-${id}`)).toBeVisible();
       }),
     );
-    console.log('has blog item list success');
   });
 });

b3a608e7c4f916f2f35ee5c7f65fad61ff3d3742
Artёm <artem_ganev@epam.com>
2024-11-17 00:32:04 +0000

    edb0939ed506a0b6df7eec22f0d359e9d02d8956

---
+++ assets/http-client-DtyeYjGE.js
@@ -0,0 +1,1 @@
+import{ah as e}from"./vendors-YUUonKjp.js";const a=e.create({headers:{"Content-Type":"application/json"},prefixUrl:"https://artemganev-be-13b50ea004b5.herokuapp.com/api",retry:0});export{a as h};

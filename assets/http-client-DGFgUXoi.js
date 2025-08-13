25197ae88037b6cab735f455f5785ba71282b9ca
Artёm <artem_ganev@epam.com>
2025-08-13 02:22:13 +0000

    c941f0e07b772a8cde7cda59887db086d40acc62

---
+++ assets/http-client-DGFgUXoi.js
@@ -0,0 +1,1 @@
+import{ah as t}from"./vendors-YUUonKjp.js";const n=t.create({headers:{"Content-Type":"application/json"},prefixUrl:"https://d1upn73nuenpav.cloudfront.net/api",retry:0});export{n as h};
